e5877c4737c923c11350217a7bb66ef4d62a1f04
Marcelo Savian <marcelo.savian@gmail.com>
2021-04-24 19:30:33 -0300

    chore: added jest and typescript

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/**/index.ts',
+    '!<rootDir>/**/*-protocols.ts',
+    '!**/test/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  },
+  preset: '@shelf/jest-mongodb'
+}
